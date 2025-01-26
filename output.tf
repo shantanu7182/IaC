5326de19ab47563b544971d991a0678136f0d289
Shantanu Chaturvedi <122782785+shantanu7182@users.noreply.github.com>
2025-01-26 21:26:20 +0000

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,59 +1,12 @@
-
-#This AWS Content is provided subject to the terms of the AWS Customer Agreement available at
-#http://aws.amazon.com/agreement or other written agreement between Customer and either
-
-
-output "codecommit_name" {
-  value       = module.codecommit_infrastructure_source_repo.repository_name
-  description = "The name of the Codecommit repository"
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
 }
 
-output "codecommit_url" {
-  value       = module.codecommit_infrastructure_source_repo.clone_url_http
-  description = "The Clone URL of the Codecommit repository"
+output "public_ip_address" {
+  value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
 }
 
-output "codecommit_arn" {
-  value       = module.codecommit_infrastructure_source_repo.arn
-  description = "The ARN of the Codecommit repository"
-}
-
-output "codebuild_name" {
-  value       = module.codebuild_terraform.name
-  description = "The Name of the Codebuild Project"
-}
-
-output "codebuild_arn" {
-  value       = module.codebuild_terraform.arn
-  description = "The ARN of the Codebuild Project"
-}
-
-output "codepipeline_name" {
-  value       = module.codepipeline_terraform.name
-  description = "The Name of the CodePipeline"
-}
-
-output "codepipeline_arn" {
-  value       = module.codepipeline_terraform.arn
-  description = "The ARN of the CodePipeline"
-}
-
-output "iam_arn" {
-  value       = module.codepipeline_iam_role.role_arn
-  description = "The ARN of the IAM Role used by the CodePipeline"
-}
-
-output "kms_arn" {
-  value       = module.codepipeline_kms.arn
-  description = "The ARN of the KMS key used in the codepipeline"
-}
-
-output "s3_arn" {
-  value       = module.s3_artifacts_bucket.arn
-  description = "The ARN of the S3 Bucket"
-}
-
-output "s3_bucket_name" {
-  value       = module.s3_artifacts_bucket.bucket
-  description = "The Name of the S3 Bucket"
+output "tls_private_key" {
+  value     = tls_private_key.example_ssh.private_key_pem
+  sensitive = true
 }
